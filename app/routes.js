62855b99638d53e3ebd0b0c7499892845c728cca
liammcmurray <liammcmurray@gmail.com>
2020-06-05 17:35:46 +0100

    updated sections so sub-sections flow

--- app/routes.js
+++ app/routes.js
@@ -4,3 +4,17 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 module.exports = router
+
+router.post('/lpa-decision-check', function (req, res) {
+  // Get the answer from session data
+  // The name between the quotes is the same as the 'name' attribute on the input elements
+  // However in JavaScript we can't use hyphens in variable names
+
+  let lpa = req.session.data['lpa-decision']
+
+  if (lpa === 'no') {
+    res.redirect('/index')
+  } else {
+    res.redirect('/upload-decision')
+  }
+})
