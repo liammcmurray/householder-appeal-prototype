e87a37b6d77a84279567e33f586a645d6d2d4d06
liammcmurray <liammcmurray@gmail.com>
2020-06-09 08:56:02 +0100

    all sections in - 6 needs formatting

--- app/routes.js
+++ app/routes.js
@@ -18,3 +18,31 @@ router.post('/lpa-decision-check', function (req, res) {
     res.redirect('/upload-decision')
   }
 })
+
+router.post('/site-ownership-check', function (req, res) {
+  // Get the answer from session data
+  // The name between the quotes is the same as the 'name' attribute on the input elements
+  // However in JavaScript we can't use hyphens in variable names
+
+  let owner = req.session.data['site-owner-names']
+
+  if (owner === 'no') {
+    res.redirect('/site-ownership-certb')
+  } else {
+    res.redirect('/index')
+  }
+})
+
+router.post('/agricultural-check', function (req, res) {
+  // Get the answer from session data
+  // The name between the quotes is the same as the 'name' attribute on the input elements
+  // However in JavaScript we can't use hyphens in variable names
+
+  let holding = req.session.data['agri-holding']
+
+  if (holding === 'yes') {
+    res.redirect('/notification-agriculture')
+  } else {
+    res.redirect('/index')
+  }
+})
